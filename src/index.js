7c3cc80845a3c18d08a5d635ffa6af6e0b54127a
Kirabo19 <kapojames@gmail.com>
2022-06-15 16:53:50 +0300

    interactive elements

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import add from './modules/add';
 import removeTask from './modules/remove';
 import { UpdateTask, saveUpdatedTask } from './modules/edit';
 import UpdateStorage from './modules/localStorage';
+import UpdateStatus from './modules/status';
 
-const list = JSON.parse(localStorage.getItem('ToDoList')) || [];
+let list = JSON.parse(localStorage.getItem('ToDoList')) || [];
 const input = document.querySelector('#input');
 const addBtn = document.querySelector('.add_btn');
 const form = document.querySelector('#list_input');
 const listTasks = document.querySelector('.list');
-
 class Task {
   constructor(value) {
     this.description = value;
@@ -18,32 +18,31 @@ class Task {
     this.index = list.length + 1;
   }
 }
-
 // display function
-
 const displayTasks = (tasks) => {
   listTasks.innerHTML = '';
   tasks.forEach((task) => {
     const li = document.createElement('li');
     li.classList.add('li', 'flex');
     listTasks.appendChild(li);
-
     // add <div>
     const div = document.createElement('div');
     div.classList.add('list_task', 'flex');
     li.appendChild(div);
-
     // add <input>
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.classList.add('checkbox');
+    checkbox.id = `box-${task.index}`;
     checkbox.checked = task.completed;
     div.appendChild(checkbox);
 
     // add <span>
     const span = document.createElement('span');
     span.innerText = task.description;
-    span.classList.add('span');
+    if (task.completed === true) {
+      span.classList.add('span', 'done');
+    } else { span.classList.add('span'); }
     div.appendChild(span);
 
     // add <img>
@@ -55,7 +54,6 @@ const displayTasks = (tasks) => {
     li.appendChild(moreBtn);
   });
 };
-
 // Add function
 const addTask = () => {
   const newTask = new Task(input.value);
@@ -64,23 +62,19 @@ const addTask = () => {
   displayTasks(list);
   input.value = '';
 };
-
 addBtn.addEventListener('click', addTask);
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   addTask();
   input.blur();
 });
-
 // remove function
 const deleteTask = (idNumer, e) => {
   removeTask(list, idNumer - 1);
-
   // Update Index
   list.forEach((task, i) => {
     task.index = i + 1;
   });
-
   // Update id
   const binBtns = document.querySelectorAll('.more_btn');
   binBtns.forEach((btn) => {
@@ -90,7 +84,6 @@ const deleteTask = (idNumer, e) => {
   });
   UpdateStorage(list);
 };
-
 listTasks.addEventListener('click', (e) => {
   if (e.target.classList.contains('more_btn')) {
     const idNumer = e.target.id;
@@ -98,13 +91,10 @@ listTasks.addEventListener('click', (e) => {
     deleteTask(idNumer, e);
   }
 });
-
 // editing Tasks
-
 const editTask = (e) => {
   UpdateTask(e);
 };
-
 listTasks.addEventListener('dblclick', (e) => {
   if (e.target.classList.contains('span')) {
     editTask(e);
@@ -123,6 +113,28 @@ listTasks.addEventListener('dblclick', (e) => {
     });
   }
 });
+
+// Update Status
+
+listTasks.addEventListener('change', (e) => {
+  if (e.target.classList.contains('checkbox')) {
+    UpdateStatus(e, list);
+    UpdateStorage(list);
+  }
+});
+
+// Clear all completed Tasks
+const clearBtn = document.querySelector('#clearBtn');
+
+clearBtn.addEventListener('click', () => {
+  const UncompeledTasks = list.filter((task) => task.completed === false);
+  UncompeledTasks.forEach((task, i) => { (task.index = i + 1); });
+  list = UncompeledTasks;
+  displayTasks(list);
+  UpdateStorage(list);
+});
+
+// Load to do list
 
 window.onload = () => {
   displayTasks(list);
